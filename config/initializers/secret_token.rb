627431660e3700694f9cda99ae988de6addf6ba2
Bill Davidheiser <billd@jobaline.com>
2012-07-30 10:01:53 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '0c6d07d64b79b7bd28b12a9406cdf4addf63cb1838ddb8548b3eeb4aa3cc57157e31320c88c8636f63fdbfb38c32e7902b864efa78ea834885bb800911f67cd5'
